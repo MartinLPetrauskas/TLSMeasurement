921268f7bcaf1743e6e630f73c550ad0c07747be
Martin Petrauskas <martin.petrauskas@protonmail.com>
2020-04-22 14:57:49 -0400

    Updated ssllabsscanner with new functions and scan_sites uses cached results if available

--- ssllabsscanner.py
+++ ssllabsscanner.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import requests
 import time
@@ -8,13 +8,14 @@ import logging
 API = 'https://api.ssllabs.com/api/v3/'
 
 
-def requestAPI(path, payload={}):
-    '''This is a helper method that takes the path to the relevant
-        API call and the user-defined payload and requests the
-        data/server test from Qualys SSL Labs.
-
-        Returns JSON formatted data'''
-
+def requestAPI(path, payload=None):
+    """
+    This is a helper function that takes the path to the relevant API call and the user defined payload and requests
+    the data/server test from Qualys SSL Labs.
+    @param path: path relevant to API call (usually 'analyze')
+    @param payload: JSON object with information about the parameters for the API call
+    @return: JSON object with results from API call
+    """
     url = API + path
 
     try:
@@ -27,41 +28,35 @@ def requestAPI(path, payload={}):
     return data
 
 
-def resultsFromCache(host, publish='off', startNew='off', fromCache='on', all='done'):
+def scan(host, fromCache):
+    """
+    This function will begin an assessment and return the results
+    @param host: hostname which will be analyzed
+    @param fromCache: determines if the API will retrieve cached assessment reports
+    @return: JSON object with API results
+    """
     path = 'analyze'
     payload = {
-                'host': host,
-                'publish': publish,
-                'startNew': startNew,
-                'fromCache': fromCache,
-                'all': all
-              }
-    data = requestAPI(path, payload)
-    return data
-
-
-def newScan(host, publish='off', startNew='on', all='done', ignoreMismatch='on'):
-    path = 'analyze'
-    payload = {
-                'host': host,
-                'publish': publish,
-                'startNew': startNew,
-                'all': all,
-                'ignoreMismatch': ignoreMismatch
-              }
+        'host': host,
+        'all': 'done',
+        'ignoreMismatch': 'on',
+    }
+    if fromCache:
+        payload.update({'fromCache': 'on', 'maxAge': 24})
+    else:
+        payload.update({'startNew': 'on'})
     results = requestAPI(path, payload)
 
-    payload.pop('startNew')
+    if not fromCache:
+        payload.pop('startNew')
 
-    # This is to see if we make too many requests, since there is rate-limiting on this API
-    # Fail-safe action is to sleep for 90 seconds before continuing
+    # There is no need to poll for the results right away since it takes 60+ seconds to get them
+    time.sleep(10)
+
+    # A variety of errors can appear while making API requests.
+    # Fail-safe option is to return if any errors appear and let the invoking program deal with them
     if 'errors' in results.keys():
         return results
-
-    # This is for when a API request just begins but does not initially have the 'status' field
-    while 'status' not in results.keys():
-        time.sleep(30)
-        results = requestAPI(path, payload)
 
     while results['status'] != 'READY' and results['status'] != 'ERROR':
         time.sleep(30)
